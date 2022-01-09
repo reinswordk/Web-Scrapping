a43c2e9e592c932f1a62f3c7334849f00377590c
cecepwahyucahyana <cecepwahyu.2019@student.uny.ac.id>
2022-01-09 21:24:27 +0700

    First Web

---
+++ lat_scraping_csv.py
@@ -0,0 +1,124 @@
+from itertools import tee
+from typing import Text
+import requests
+
+from pymongo import MongoClient
+import pymongo
+import csv
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_dbconnection():
+    myclient = pymongo.MongoClient('mongodb://localhost:27017/') #link mongoDB, note: koneksikan terlebih dahulu
+    mydb = myclient['latscrapdb'] #gantinama database kalian, kalau belum ada/belum buat bisa langsung dibuat disini contoh => mydb = myclient['databaseku']
+
+    mycol = mydb['bookapp_book'] #nama collection atau table, kalau belum ada lakukan sama seperti sebelumnya
+    
+    return mycol
+
+
+# print(var_in.inserted_id)
+def save_data(masukin):
+    mycol = get_dbconnection()
+    var_in = mycol.insert_one(masukin)
+    return var_in
+    
+        
+def print_data(mycol):
+    #mycol = get_dbconnection
+    for x in mycol.find():
+        print(x)
+
+def scrap_googlebook(url):
+    global book_info
+    urlnya = url
+    page = requests.get(urlnya)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    _judul = soup.select('h1[itemprop="name"] span')
+    title = _judul[0].getText()
+
+    _penulis = soup.select('a[class="hrTbp R8zArc"]')
+    author = _penulis[0].getText()
+    
+    _desc = soup.select('span[jsslot=""]')
+    summary = _desc[0].getText()
+
+    _cover = soup.find('img', 'T75of h1kAub')
+    cover_image = _cover['src']
+    
+    halaman = soup.find("div", string='Pages').find_next_sibling().text
+    pub_date = ''
+    try:
+        pub_date = soup.find("div", string='Published on').find_next_sibling().text
+        #print(pub_date)
+    except AttributeError:
+        print("Tanggal terbit tidak ditemukan")
+    
+    penerbit = soup.find("div", string='Publisher').find_next_sibling().text
+    
+    bahasa = soup.find("div", string='Language').find_next_sibling().text
+    
+    kompability = soup.find("div", string='Best for').find_next_sibling().text
+    
+    genre = soup.find("div", string='Genres').find_next_sibling().text
+    
+    harga_1 = soup.select('button[class="LkLjZd ScJHi HPiPcc IfEcue"]  meta[itemprop="price"]')[0]['content'].replace('$','')
+    harga_2 = float(harga_1) * 14266.00
+    harga_final = 'Rp ' + "{:,}".format(int(harga_2)) + ',00'
+ 
+    _rating = soup.select('div[class="BHMmbe"]')
+    rating = _rating[0].getText()
+
+    _jumlah_rating = soup.select('span[class="EymY4b"] span')
+    jumlah_rating = _jumlah_rating[1].getText()
+    
+
+    book_info = {
+        'cover_image':cover_image,
+        'title':title,
+        'author':author,
+        'summary':summary,
+        'Penerbit':penerbit,
+        'Tanggal Terbit':pub_date,
+        'Bahasa':bahasa,
+        'Halaman':halaman,
+        'Baik Untuk':kompability,
+        'Genre':genre,
+        'Harga':harga_final,
+        'Rating':rating,
+        'Jumlah Rating':jumlah_rating
+    }
+    # print(book_info)
+    return book_info
+
+def import_csv(filename):
+    global book_info_csv
+    with open(filename, 'r') as csvfile:
+	
+        csvreader = csv.reader(csvfile)
+        
+        book_info_csv = {rows[0]:rows[1] for rows in csvreader}
+    return book_info_csv
+
+def main():
+    choice = input("mau (s)scraping atau (c)import csv: ")
+    if choice == 's':
+        alamat = input('Masukan URL buku: ')
+        scrap_googlebook(alamat)
+        save_data(book_info)
+        print("Berhasil, cek database wis mlebu rung")
+        # mycol = get_dbconnection()
+        # # print_data(mycol)
+    else:
+        file_csv = "databuku_example.csv"
+        import_csv(file_csv)
+        save_data(book_info_csv)
+        print("Berhasil, cek database wis mlebu rung")
+       
+          
+if __name__ == '__main__':
+    main()
+
